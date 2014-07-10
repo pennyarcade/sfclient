c976dbb3ea317976909e9e36b8fa6a9a6e9ebf94
chocokiko <chocokiko@gmx.de>
2014-07-10 09:49:04 +0200

    fix pylint issues

---
+++ sflegacyfonts.py
@@ -0,0 +1,495 @@
+#!/usr/bin/python
+# coding=utf-8
+
+'''
+    Shakes & Fidget command line client
+
+    by Chocokiko
+
+    Module for obsolete legacy stuff
+'''
+
+from sflegacy import BaseClass
+
+
+class FontFormat(BaseClass):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatToiletAura(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackErrorHalf(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackErrorOnlineHalf(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatError(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatDefault(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHighStakes(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHighStakesHighLight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHighStakesHighLightGrayed(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatBook(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatBookHint(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatBookLeft(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatBullshit(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatAttackLabel(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatSpeech(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGrayed(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGrayedHighLight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHallListHeading(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildHallNoAttack(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatClassError(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatDefaultLeft(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHallListText(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHallListHighLight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatAttribBonus(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatAttribTemp(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatAttrib(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPayIcon(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListHeading(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListText(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListTextSys(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListText(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextOnline(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackError(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackErrorOnline(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackErrorOnlinePopup(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackOk(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildListTextAttackOkPopup(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListHighLight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListHighLightSys(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListTextSysRed(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListHighLightSysRed(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListTextSysGreen(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPostListHighLightSysGreen(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatQuestBar(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatTimeBar(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatLifeBar(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatDamage(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatCriticalDamage(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatCatapultDamage(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatChat(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatChatWhisper(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatChatError(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildBuilding(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatGuildMoney(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHighlight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHighlightWhisper(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatHeading(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatScreenTitle(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopup(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompare(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompareSum(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompareBetter(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompareWorse(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompareBetterHL(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatPopupCompareWorseHL(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatEpicItemQuote(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatItemEnchantment(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatLOGoutLink(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
+
+
+class FontFormatLOGoutLinkHighLight(FontFormat):
+    '''
+        obsolete with cli?
+    '''
+    pass
