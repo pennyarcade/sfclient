08962f3ae411f73826333e542a1e46ee6f8476f5
chocokiko <chocokiko@gmx.de>
2014-07-14 16:50:31 +0200

    fix pylint/pep8 issues

--- classes/sfface.py
+++ classes/sfface.py
@@ -10,6 +10,10 @@
 
 '''
 
+import random
+
+from sfglobals import C
+
 
 class Face(object):
     '''
@@ -34,3 +38,91 @@ class Face(object):
         self.special = special
         self.special2 = special2
         self.volk = volk
+        self.klasse_gewaehlt = bool(param_obj["playerclass"])
+
+    def randomize_character(self):
+        self.volk = int(random.random() * 8) + 1
+        self.male = random.random() > 0.5
+
+        if param_obj["playerclass"]:
+            self.cclass = int(param_obj["playerclass"])
+            if self.cclass < 1:
+                self.cclass = 1
+            if self.cclass > 3:
+                self.cclass = 3
+            self.klasse_gewaehlt = True
+        else:
+            self.cclass = int(random.random() * 3) + 1
+            self.klasse_gewaehlt = False
+
+        self.randomize_char_image()
+
+    def randomize_char_image(self):
+        # var ColorOffset:* = function (ItemID){
+        #     if ((get_char_image_bound(char_volk, char_male, 11) & ItemID)){
+        #         return ((char_color * 100));
+        #     };
+        #     return (0);
+        # };
+        '''
+        self.color = int(random.random() * self.get_char_image_bound(10) + 1)
+        self.mouth = int(random.random() * self.get_char_image_bound(1) + 1)
+        self.beard = int(random.random() * self.get_char_image_bound(2) + 1
+                         + ColorOffset(C['BEARD']))
+        self.nose = int(random.random() * self.get_char_image_bound(3) + 1)
+        self.eyes = int(random.random() * self.get_char_image_bound(4) + 1)
+        self.brows = int(random.random() * get_char_image_bound(5) + 1
+                         + ColorOffset(C['BROWS'])
+        # self.ears = int(((random.random() * get_char_image_bound(char_volk,
+        #                 char_male, 6)) + 1));
+        # self.hair = (int(((random.random() * get_char_image_bound(char_volk,
+        #              char_male, 7)) + 1)) + ColorOffset(C_HAIR));
+        # self.special = int(((random.random() * get_char_image_bound(char_volk,
+        #                    char_male, 8)) + 1));
+        # self.special2 = (int(((random.random() * get_char_image_bound(char_volk,
+        #                  char_male, 9)) + 1)) + ColorOffset(C_SPECIAL2));
+        '''
+        # load_character_image()
+
+    def get_char_image_bound(self, item_index):
+        '''
+            lookup image boundary?
+        '''
+        lookup = {
+            True: {
+                1: [None, 9, 13, 6, 7, 7, 5, 11, 17, 0, 5,
+                    (C['BROWS'] + C['HAIR'] + C['BEARD'])],
+                2: [None, 8, 7, 7, 8, 5, 4, 10, 13, 0, 3,
+                    (C['BROWS'] + C['HAIR'] + C['BEARD'])],
+                3: [None, 5, 5, 5, 8, 5, 10, 13, 0, 5,
+                    (C['BROWS'] + C['HAIR'] + C['BEARD'])],
+                4: [None, 10, 12, 6, 9, 6, 12, 17, 0, 5,
+                    (C['BROWS'] + C['HAIR'] + C['BEARD'])],
+                5: [None, 7, 5, 5, 6, 5, 5, 10, 9, 0, 5,
+                    (C['HAIR'] + C['BEARD'])],
+                6: [None, 6, 6, 5, 5, 5, 5, 8, 12, 0, 3,
+                    (C['BROWS'] + C['HAIR'] + C['BEARD'])],
+                7: [None, 6, 8, 5, 9, 6, 5, 12, 12, 0, 0, 0],
+                8: [None, 9, 10, 6, 7, 5, 5, 0, 17, 11, 5,
+                    (C['BEARD'] + C['SPECIAL2'])],
+            },
+            False: {
+                1: [None, 8, 0, 6, 6, 6, 6, 8, 5, 0, 4],
+                2: [None, 8, 0, 8, 7, 8, 7, 8, 5, 0, 4],
+                3: [None, 9, 0, 6, 6, 8, 4, 8, 4, 0, 4],
+                4: [None, 7, 0, 7, 6, 6, 6, 8, 5, 0, 4,
+                    (C['HAIR'] + C['BROWS'])],
+                5: [None, 7, 0, 5, 6, 5, 4, 8, 7, 0, 4, C['HAIR']],
+                6: [None, 9, 0, 6, 6, 6, 3, 6, 5, 0, 3, C['HAIR']],
+                7: [None, 9, 0, 5, 6, 5, 4, 8, 4, 0, 4, C['HAIR']],
+                8: [None, 8, 5, 7, 6, 5, 4, 7, 6, 4, 3, C['HAIR']],
+            }
+        }
+
+        try:
+            if lookup[self.male][self.volk][item_index] is not None:
+                return lookup[self.male][self.volk][item_index]
+            else:
+                return 0
+        except IndexError:
+            return 0
--- classes/sftoilet.py
+++ classes/sftoilet.py
@@ -20,3 +20,146 @@ class Toilet(object):
             setup toilet object
         '''
         pass
+
+
+def toilet_tank_adjust_event():
+    '''
+    actor[(TOILET + 1)].y = ((190 + 122) - (toiletTankCurrent * 118));
+    if (toiletTankCurrent > (toiletTankDest + 0.01)){
+        toiletTankCurrent = (toiletTankCurrent - 0.01);
+    } else {
+        if (toiletTankCurrent < (toiletTankDest - 0.01)){
+            toiletTankCurrent = (toiletTankCurrent + 0.01);
+        } else {
+            toiletTankCurrent = toiletTankDest;
+            toiletTankAdjustTimer.stop();
+        };
+    };
+    '''
+    pass
+
+
+def show_toilet(
+        is_full, toilet_level, toilet_exp, toilet_max_exp, item_added=-1
+):
+    '''
+    var doShowToilet:* = None;
+    var is_full:* = is_full;
+    var toilet_level:* = toilet_level;
+    var toilet_exp:* = toilet_exp;
+    var toilet_max_exp:* = toilet_max_exp;
+    var item_added = item_added;
+    doShowToilet = function (buildScreen=True){
+        var i:* = 0;
+        var toiletItemAddFrame:* = 0;
+        var toiletItemAddTimer:* = None;
+        var gatheredItemId:* = 0;
+        var itemDestX:* = NaN;
+        var itemDestY:* = NaN;
+        var toiletItemAddFrameEvent:* = None;
+        var buildScreen:Boolean = buildScreen;
+        toiletTankDest = (toilet_exp / toilet_max_exp);
+        toiletTankAdjustTimer.stop();
+        if (buildScreen){
+            toiletTankCurrent = toiletTankDest;
+            remove(CHAR_RIGHTPANE);
+            add(SCREEN_TOILET);
+            hide(TOILET_OVERLAYS);
+            show(TOILET_CHAIN);
+        };
+        display_inventory(None, True);
+        enable_popup(
+            CA_TOILET_TANK,
+            texts[TXT_TOILET_HINT].split(
+                "%1").join(str(int((toiletTankDest * 100)))).split(
+                "%2").join(str(toilet_exp)).split(
+                "%3").join(str(toilet_max_exp))
+        );
+        if (is_full == 0){
+            hide(TOILET_IDLE);
+        } else {
+            show(TOILET_IDLE);
+        };
+        hide(TOILET_DROP);
+        actor[LBL_TOILET_AURA].text = texts[(TXT_TOILET_HINT + 4)].split(
+            "#").join(chr(13)).split("%1").join(str(toilet_level));
+        actor[LBL_TOILET_AURA].x = (
+            (SCR_SHOP_BG_X + 248) - (actor[LBL_TOILET_AURA].text_width / 2));
+        toilet_tank_adjust_event();
+        if (toiletTankDest != toiletTankCurrent){
+            toiletTankAdjustTimer.start();
+        };
+        if (item_added >= 0){
+            toiletItemAddFrameEvent = function (evt:TimerEvent){
+                if ((((toiletItemAddFrame >= 50)) or (!(on_stage(TOILET))))){
+                    actor[gatheredItemId].x = itemDestX;
+                    actor[gatheredItemId].y = itemDestY;
+                    actor[gatheredItemId].alpha = 1;
+                    toiletItemAddTimer.stop();
+                    toiletItemAddTimer.remove_event_listener(
+                       TimerEvent.TIMER, toiletItemAddFrameEvent
+                    );
+                } else {
+                    if (toiletItemAddFrame >= 35){
+                        actor[gatheredItemId].alpha = 1;
+                        actor[gatheredItemId].x = (
+                            (actor[gatheredItemId].x + itemDestX) / 2);
+                        actor[gatheredItemId].y = (
+                            (actor[gatheredItemId].y + itemDestY) / 2);
+                    } else {
+                        actor[gatheredItemId].alpha = (toiletItemAddFrame / 35)
+                        actor[gatheredItemId].y = (actor[gatheredItemId].y - 5)
+                    };
+                };
+                i = 0;
+                while (i < 7) {
+                    hide((TOILET_FLUSH + i));
+                    i++;
+                };
+                show((TOILET_FLUSH + int(((toiletItemAddFrame / 50) * 7))));
+                toiletItemAddFrame++;
+            };
+            toiletItemAddFrame = 0;
+            toiletItemAddTimer = new Timer(25);
+            play(SND_TOILET_FLUSH);
+            toiletItemAddTimer.add_event_listener(
+                TimerEvent.TIMER, toiletItemAddFrameEvent
+            );
+            toiletItemAddTimer.start();
+            gatheredItemId = (CHAR_SLOT_11 + item_added);
+            Switch (item_added){
+                if case(0:
+                    itemDestX = CHAR_SLOTS_LEFT_X;
+                    itemDestY = CHAR_SLOTS_ROW5_Y;
+                    break;
+                if case(1:
+                    itemDestX = CHAR_SLOTS_R5C2_X;
+                    itemDestY = CHAR_SLOTS_ROW5_Y;
+                    break;
+                if case(2:
+                    itemDestX = CHAR_SLOTS_R5C3_X;
+                    itemDestY = CHAR_SLOTS_ROW5_Y;
+                    break;
+                if case(3:
+                    itemDestX = CHAR_SLOTS_R5C4_X;
+                    itemDestY = CHAR_SLOTS_ROW5_Y;
+                    break;
+                if case(4:
+                    itemDestX = CHAR_SLOTS_RIGHT_X;
+                    itemDestY = CHAR_SLOTS_ROW5_Y;
+                    break;
+            };
+            actor[gatheredItemId].alpha = 0;
+            actor[gatheredItemId].x = (SCR_SHOP_BG_X + 205);
+            actor[gatheredItemId].y = 590;
+        };
+    };
+    if (on_stage(TOILET)){
+        doshow_toilet(False);
+        return;
+    };
+    load(SCREEN_TOILET);
+    show_character_screen();
+    when_loaded(doShowToilet);
+    '''
+    print is_full, toilet_level, toilet_exp, toilet_max_exp, item_added
